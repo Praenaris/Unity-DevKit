8cb26aa90ea41c0405264a106570a9d01b19d853
Tabernero <16870364+davidtabernerom@users.noreply.github.com>
2026-09-03 17:44:49 +0200

    Optimize the transform and monobehaviour children listing

--- Runtime/Behaviours/OpossumBehaviour.cs
+++ Runtime/Behaviours/OpossumBehaviour.cs
@@ -1,6 +1,7 @@
 #if UNITY_NGO
 
 
+using DragonResonance.Extensions;
 using System.Collections.Generic;
 using System.Linq;
 using Unity.Netcode;
@@ -58,8 +59,8 @@ namespace DragonResonance.Behaviours
 		#region Properties
 
 			public RectTransform rectTransform => (RectTransform)base.transform;
-			public IEnumerable<Transform> children => from object child in base.transform select child as Transform;
-			public IEnumerable<RectTransform> rectChildren => from object child in base.transform select child as RectTransform;
+			public IEnumerable<Transform> children => base.transform.GetChildren();
+			public IEnumerable<RectTransform> rectChildren => base.transform.GetRectChildren();
 
 		#endregion
 
@@ -82,19 +83,15 @@ namespace DragonResonance.Behaviours
 #endif
 
 
-/*       ________________________________________________________________       */
-/*           _________   _______ ________  _______  _______  ___    _           */
-/*           |        \ |______/ |______| |  _____ |       | |  \   |           */
-/*           |________/ |     \_ |      | |______| |_______| |   \__|           */
-/*           ______ _____ _____ _____ __   _ _____ __   _ _____ _____           */
-/*           |____/ |____ [___  |   | | \  | |___| | \  | |     |____           */
-/*           |    \ |____ ____] |___| |  \_| |   | |  \_| |____ |____           */
-/*       ________________________________________________________________       */
-/*                                                                              */
-/*           David Tabernero M.  <https://github.com/davidtabernerom>           */
-/*           Dragon Resonance    <https://github.com/dragonresonance>           */
-/*                  Copyright © 2021-2026. All rights reserved.                 */
-/*                Licensed under the Apache License, Version 2.0.               */
-/*                         See LICENSE.md for more info.                        */
-/*       ________________________________________________________________       */
-/*                                                                              */
+/*                                                                                                                */
+/*       `7MM"""Mq.`7MM"""Mq.       db     `7MM"""YMM  `7MN.   `7MF'     db     `7MM"""Mq. `7MMF' .M"""bgd        */
+/*         MM   `MM. MM   `MM.     ;MM:      MM    `7    MMN.    M      ;MM:      MM   `MM.  MM  ,MI    "Y        */
+/*         MM   ,M9  MM   ,M9     ,V^MM.     MM   d      M YMb   M     ,V^MM.     MM   ,M9   MM  `MMb.            */
+/*         MMmmdM9   MMmmdM9     ,M  `MM     MMmmMM      M  `MN. M    ,M  `MM     MMmmdM9    MM    `YMMNq.        */
+/*         MM        MM  YM.     AbmmmqMA    MM   Y  ,   M   `MM.M    AbmmmqMA    MM  YM.    MM  .     `MM        */
+/*         MM        MM   `Mb.  A'     VML   MM     ,M   M     YMM   A'     VML   MM   `Mb.  MM  Mb     dM        */
+/*       .JMML.    .JMML. .JMM.AMA.   .AMMA.JMMmmmmMMM .JML.    YM .AMA.   .AMMA.JMML. .JMM.JMML.P"Ybmmd"         */
+/*                                                                                                                */
+/*                 Licensed under the Apache License, Version 2.0.  See LICENSE.md for more info.                 */
+/*                                     Copyright © 2026. All rights reserved.                                     */
+/*                                                                                                                */
